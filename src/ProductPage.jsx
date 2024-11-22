4ffeb9ce6fe2401068a5b4e779ccb0596eacba7f
beejayofgod <adexbolaji100@gmail.com>
2024-11-22 14:06:35 +0100

    removed hover state for add to cart btn

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -292,7 +292,7 @@ const ProductPage = (props) => {
                 changeCartNo(quantity);
                 localStorage.setItem("quantity", quantity + 1);
               }}
-              className="bg-primary-orange hover:bg-opacity-50 basis-[65%] min-w-fit rounded-md py-4 flex items-center justify-center gap-4 mb-4 md:mb-0"
+              className="bg-primary-orange md:hover:bg-opacity-50 basis-[65%] min-w-fit rounded-md py-4 flex items-center justify-center gap-4 mb-4 md:mb-0"
             >
               <span>
                 <svg
